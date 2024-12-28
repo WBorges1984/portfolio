7406efcc1ce4fbb2a53ccbf11010b78a7781aa65
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2024-12-28 00:42:04 -0300

    Pag sobre mim

--- src/components/sobreMim/SobreMim.jsx
+++ src/components/sobreMim/SobreMim.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import'./sobreMim.style.css'
+import img from '../../assets/img/sobremim.png'
 
 export default function Sobremim() {
   return (
-    <div>Sobremim</div>
+    <section id='sobremim'>
+      <article className='imgSobreMim'>
+        {/* <img src="https://my-portfolio-lp.vercel.app/assets/about-illustration.svg" alt="" srcset="" /> */}
+        <img src={img} width="450" alt="" />
+      </article>
+      <article className='txtSobreMim'>
+        <h1>Sobre Mim</h1>
+        <h3>
+        Meu nome é Willian Borges, tenho 40 anos e sou graduando em Análise e 
+        Desenvolvimento de Sistemas pela Universidade Cândido Mendes, com 
+        conclusão prevista para abril de 2025. Atuo na área de tecnologia 
+        desde 2019, desenvolvendo sites, aplicativos móveis e sistemas completos.
+        </h3>
+        <h3>
+        Iniciei na programação com HTML, CSS e JavaScript, ampliando meu 
+        conhecimento para <span>TypeScript</span>, <span>ReactJS</span>, <span>NodeJS</span> e <span>React Native</span>. 
+        Tenho experiência em Front-End e Back-End, trabalhando com bancos de dados 
+        relacionais e NoSQL. Atualmente, aprimoro habilidades com Tailwind, 
+        Material UI e MySQL, focando no desenvolvimento fullstack para criar 
+        soluções tecnológicas de alto impacto.
+        </h3>
+        
+      </article>
+    </section>
   )
 }
