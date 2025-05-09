854ec96c0988534d74f088a8cb3ddef2832949a7
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2025-05-09 11:35:29 -0300

    AJUSTE LOGO E FAVICON

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,12 @@
 import './headerStyle.css'
 import { useMenu } from "../context/menuContext";
-import logo from '../../assets/img/logo.png'
+import logo from '../../assets/img/logoWillDev.png'
 
 export default function Header(){
   const {menu, setMenu} = useMenu();
     return(
         <header style={{display:'flex'}}>
-        <span><img src={logo} alt="" width={80}/></span>
+        <span><img src={logo} alt="" width={60} style={{border: '1px solid #2b4a63', borderRadius: '10px'}}/></span>
          <nav>
            <button onClick={()=>setMenu('home')}>Início</button>
            <button onClick={()=>setMenu('sobre')}>Sobre mim</button>
